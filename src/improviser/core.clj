0d0089d65969e46a916c3d94db674c03b4b8585d
Paul Bowler <paul_bowler@yahoo.com>
2013-11-09 13:45:21 +0000

    Fixed namespace error

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -6,19 +6,18 @@
 
 (def phat-synth (m/midi-out))
 
-
 (def ax (m/midi-in "axiom"))
 
 ; Connect ins and outs easily
-(m/midi-route m/keyboard m/phat-synth)
+(m/midi-route keyboard phat-synth)
 
 ; Trigger a note (note 40, velocity 100)
 (m/midi-note-on phat-synth 40 100)
 (Thread/sleep 500)
-(m/midi-note-off m/phat-synth 0)
+(m/midi-note-off phat-synth 0)
 
 ; Or the short-hand version to start and stop a note
-(m/midi-note m/phat-synth 40 100 500)
+(m/midi-note phat-synth 40 100 500)
 
 ; And the same thing with a sequence of notes
-(m/midi-play m/phat-synth [40 47 40] [80 50 110] [250 500 250])
+(m/midi-play phat-synth [40 47 40] [80 50 110] [250 500 250])
