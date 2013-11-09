efc61cddaf3a1de6b355b5c445ec32c5e9cf0bfd
Paul Bowler <paul_bowler@yahoo.com>
2013-11-09 15:10:44 +0000

    Fixed note-off issues

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -6,18 +6,16 @@
 
 (def phat-synth (m/midi-out))
 
-(def ax (m/midi-in "axiom"))
-
 ; Connect ins and outs easily
 (m/midi-route keyboard phat-synth)
 
 ; Trigger a note (note 40, velocity 100)
 (m/midi-note-on phat-synth 40 100)
 (Thread/sleep 500)
-(m/midi-note-off phat-synth 0)
+(m/midi-note-off phat-synth 40)
 
 ; Or the short-hand version to start and stop a note
 (m/midi-note phat-synth 40 100 500)
 
 ; And the same thing with a sequence of notes
-(m/midi-play phat-synth [40 47 40] [80 50 110] [250 500 250])
+(m/midi-play phat-synth [30 41 40 55 42 34] [80 50 20 40 50 80] [250 500 250 250 500 250])
