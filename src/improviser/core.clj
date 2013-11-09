97f84a2a0411a3b633965ed586c99fe99e3c4590
Paul Bowler <paul_bowler@yahoo.com>
2013-11-09 13:41:24 +0000

    Added test code for midi functions

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -1,6 +1,24 @@
-(ns improviser.core)
+(ns improviser.core
+  (:require [overtone.midi :as m]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def keyboard (m/midi-in))
+
+(def phat-synth (m/midi-out))
+
+
+(def ax (m/midi-in "axiom"))
+
+; Connect ins and outs easily
+(m/midi-route m/keyboard m/phat-synth)
+
+; Trigger a note (note 40, velocity 100)
+(m/midi-note-on phat-synth 40 100)
+(Thread/sleep 500)
+(m/midi-note-off m/phat-synth 0)
+
+; Or the short-hand version to start and stop a note
+(m/midi-note m/phat-synth 40 100 500)
+
+; And the same thing with a sequence of notes
+(m/midi-play m/phat-synth [40 47 40] [80 50 110] [250 500 250])
