71ff53a2912922a7c22ca2f1dbf8c08f14b7c62c
Paul Bowler <paul_bowler@yahoo.com>
2013-11-10 14:49:32 +0000

    Added support for Yamaha CP5

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -1,17 +1,17 @@
 (ns improviser.core
   (:require [overtone.midi :as m]))
 
-(def synth (m/midi-out "IAC Bus 2"))
+(map :description (m/midi-ports))
+
+(def synth (m/midi-out "IAC"))
 
 (defn midi-chord
   "Play a seq of notes with the corresponding velocities and durations."
   [out notes velocity duration]
-  (map #(m/midi-note out % velocity duration) notes))
+  (map #(m/midi-note out % velocity duration 0) notes))
 
 (def mid-C 60)
-(def dom7 [4 6 9 10 14])
+(def dom7 [-12 4 6 10 14])
 (def c7 (map #(+ mid-C %) dom7))
 
-(midi-chord synth c7 80 1000)
-
-(m/midi-ports)
+(midi-chord synth c7 70 1000)
