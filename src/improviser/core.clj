9f9101c84815713e906de55b6618428ebaae4a67
Paul Bowler <paul_bowler@yahoo.com>
2013-11-10 15:06:04 +0000

    Added second chord to test

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -11,7 +11,11 @@
   (map #(m/midi-note out % velocity duration 0) notes))
 
 (def mid-C 60)
-(def dom7 [-12 4 6 10 14])
+(def F3 53)
+(def dom7 [0 4 7 10 14 18 21])
+(def M7 [0 4 7 11 14 17 21])
 (def c7 (map #(+ mid-C %) dom7))
+(def fM7 (map #(+ F3 %) M7))
 
-(midi-chord synth c7 70 1000)
+(midi-chord synth c7  70 1000)
+(midi-chord synth fM7 70 1000)
