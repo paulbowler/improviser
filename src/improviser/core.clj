c2aab88f616f87a72c00be05215018e8799180ad
Paul Bowler <paul_bowler@yahoo.com>
2013-11-14 14:25:44 +0000

    Added chords, notes and chord tones

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -2,14 +2,48 @@
   (:use improviser.pitch)
   (:require [overtone.midi :as m]))
 
-(def *tempo* 150)
+(def *tempo* 200)
 (def *octave* 3)
 
 (distinct (map :description (m/midi-ports)))
 
 (def synth (m/midi-out "IAC"))
 
-;; (m/midi-play synth [(chord :D4 :m9)(chord :g3 :9)(chord :c4 :6*9)] [80 80 80] [1000 1000 2000])
+(def NOTE {:C  0  :c  0  :b# 0  :B# 0
+           :C# 1  :c# 1  :Db 1  :db 1  :DB 1  :dB 1
+           :D  2  :d  2
+           :D# 3  :d# 3  :Eb 3  :eb 3  :EB 3  :eB 3
+           :E  4  :e  4
+           :E# 5  :e# 5  :F  5  :f  5
+           :F# 6  :f# 6  :Gb 6  :gb 6  :GB 6  :gB 6
+           :G  7  :g  7
+           :G# 8  :g# 8  :Ab 8  :ab 8  :AB 8  :aB 8
+           :A  9  :a  9
+           :A# 10 :a# 10 :Bb 10 :bb 10 :BB 10 :bB 10
+           :B  11 :b  11 :Cb 11 :cb 11 :CB 11 :cB 11})
+
+(def CHORDS
+  (let [major7 #{0 4 7 11 14 18 21}
+        dom7   #{0 4 7 10 14 18 21}
+        hdim7  #{0 4 6 10 14 18 21}
+        minor7 #{0 3 7 10 14 17 21}
+        aug7   #{0 4 8 10 14 17 21}
+        dim7   #{0 3 6 9}]
+    {:M7         major7
+     :m7         minor7
+     :7          dom7
+     :a7         aug7
+     :d7         dim7}))
+
+(:M7 CHORDS)
+(:E NOTE)
+
+(map #(+ (:E NOTE) %) (:M7 CHORDS))
+
+(defn chord-tones [note chord]
+  (map #(+ (note NOTE) %) (chord CHORDS)))
+
+(chord-tones :G :m7)
 
 (def MIDI-CHORD-RE-STR "([A-G][#b]?)([m|M|d|a]?)([6|7|9]?)([A-G][#b]?)?:([0-9]+)" )
 (def MIDI-CHORD-RE (re-pattern MIDI-CHORD-RE-STR))
@@ -32,15 +66,15 @@
                    " does not appear to be in the required format i.e. C#M7:4"))))
     matches))
 
-(defn chord-info
+(defn generate-midi-chords
   "Takes a string representing a chord such as CM7:8 and returns a map of chord info"
   [chord-string]
   (let [[match pitch style tension root beats] (validate-chord-string! chord-string)
-        beats                               (Integer. beats)
-        pitch-octave                         (keyword (str pitch *octave*))
-        chord-type                           (keyword (str style tension))
-        duration                             (* *tempo* (Integer. beats))
-        bass-note                            (or (keyword root) (keyword pitch))]
+        beats                                  (Integer. beats)
+        pitch-octave                           (keyword (str pitch *octave*))
+        chord-type                             (keyword (str style tension))
+        duration                               (* *tempo* (Integer. beats))
+        bass-note                              (or (keyword root) (keyword pitch))]
     {:match     match
      :pitch     pitch-octave
      :type      chord-type
@@ -62,7 +96,6 @@
                :D7:4 :Dm7:4 :G7:4 :CM7:2
                :Eb7:2 :AbM7:2 :D7:2])
 
-(map chord-info LADYBIRD)
+(map generate-midi-chords LADYBIRD)
 
-(m/midi-play-song synth (map chord-info LADYBIRD))
-
+(m/midi-play-song synth (map generate-midi-chords LADYBIRD))
