42141878be71bdc1b14d3aa3f24c0423ba54809b
Paul Bowler <paul_bowler@yahoo.com>
2013-11-14 11:57:24 +0000

    Added chord map standard

--- src/improviser/core.clj
+++ src/improviser/core.clj
@@ -6,4 +6,56 @@
 
 (def synth (m/midi-out "IAC"))
 
-(m/midi-play synth [(chord :d3 :m9)(chord :g3 :9)(chord :c4 :6*9)] [80 80 80] [1000 1000 2000])
+;; (m/midi-play synth [(chord :D4 :m9)(chord :g3 :9)(chord :c4 :6*9)] [80 80 80] [1000 1000 2000])
+
+(def MIDI-CHORD-RE-STR "([A-G][#b]?)([m|M|d|a]?)([6|7|9]?)([A-G][#b]?)?:([0-9]+)" )
+(def MIDI-CHORD-RE (re-pattern MIDI-CHORD-RE-STR))
+(def ONLY-MIDI-CHORD-RE (re-pattern (str MIDI-CHORD-RE-STR)))
+
+(defn chord-matcher
+  "Determines whether a midi keyword is valid or not. If valid,
+  returns a regexp match object"
+  [mk]
+  (re-find ONLY-MIDI-CHORD-RE (name mk)))
+
+(defn validate-chord-string!
+  "Throws a friendly exception if midi-keyword mk is not
+  valid. Returns matches if valid."
+  [mk]
+  (let [matches (chord-matcher mk)]
+    (when-not matches
+      (throw (IllegalArgumentException.
+              (str "Invalid midi-string. " mk
+                   " does not appear to be in the required format i.e. C#M7:4"))))
+    matches))
+
+(defn chord-info
+  "Takes a string representing a chord such as CM7:8 and returns a map of chord info"
+  [chord-string]
+  (let [[match pitch style tension root dur] (validate-chord-string! chord-string)
+        pitch-octave                         (keyword (str pitch 0))
+        chord-type                           (keyword (str style tension))
+        duration                             (* 300 (Integer. dur))
+        bass-note                            (or (keyword root) (keyword pitch))]
+    {:match     match
+     :pitch     pitch-octave
+     :type      chord-type
+     :root      bass-note
+     :notes     (chord pitch-octave chord-type)
+     :duration  duration}))
+
+(validate-chord-string! :Dm7:2)
+
+(str "C" 4)
+
+(chord-info :Dm7A:2)
+
+(def LADYBIRD [:CM9:8 :Fm7:4 :Bb7:4 :CM7:8
+               :Bbm7:4 :Eb7:4 :AbM7:8 :Am7:4
+               :D7:4 :Dm7:4 :G7:4 :CM7:2
+               :Eb7:2 :AbM7:2 :D7:2])
+
+(map chord-info LADYBIRD)
+
+;; (m/midi-play-song synth (map chord-info LADYBIRD))
+
